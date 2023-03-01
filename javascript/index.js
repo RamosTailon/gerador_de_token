623ef44a59a4c44332d5e76e3b108172c41168c4
Tailon Ramos <tailonramos11@gmail.com>
2023-03-01 20:27:31 -0300

    função random token

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,26 @@
 const test = document.querySelector("h1");
 
-test.innerHTML = "oi";
+const listString = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+const randomIndex = () => {
+  var numberToTen = Math.floor(Math.random() * 10);
+  return numberToTen;
+};
+
+const randomToken = () => {
+  let tList = [];
+  for (let i = 0; i < 5; i++) {
+    if (i == 2 || i == 4) {
+      tList.push(listString[randomIndex()]);
+    } else {
+      tList.push(randomIndex());
+    }
+  }
+  const tokenList = tList.join().replaceAll(",", "");
+
+  console.log(tokenList);
+
+  return tokenList;
+};
+
+test.innerHTML = randomToken();
